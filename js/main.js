4acf2a4943b487240a50d72e80569c70f3698de9
enzo <enzovcnt@gmail.com>
2024-12-14 19:02:20 +0100

    test enter the messenger after login

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-alert("Hello World!");
+let token = null
+
+async function login() {
+    let params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: 'enzo',
+            password: 'abRULSboZD'
+        })
+    }
+    return await fetch('https://b1messenger.esdlyon.dev/login', params)
+    .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            return json.token
+        })
+}
+
+function enterMessenger() {
+    const username = document.querySelector('.inputUsername')
+    const password = document.querySelector('.inputPassword')
+    const logBtn = document.querySelector('.loginBtn')
+    let pTest = document.querySelector('.test')
+    let logPage = document.querySelector('.loginPage')
+    logBtn.addEventListener('click', () => {
+        login(username.value = 'enzo', password.value = 'abRULSboZD')
+            .then((data) => {
+            token = data
+                console.log(data)
+            pTest.classList.remove('d-none')
+                pTest.classList.add('d-block')
+                logPage.classList.remove('d-block')
+                logPage.classList.add('d-none')
+        })
+    })
+}
+enterMessenger()
